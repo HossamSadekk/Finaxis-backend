7a615953d506c0089577e82e4adb8e0254bb6bac
Hossam Ali <64331041+HossamAli999@users.noreply.github.com>
2024-07-25 02:31:07 +0300

    Adding Main Files

---
+++ src/main/java/com/finaxis/finaxis/FinaxisApplication.java
@@ -0,0 +1,13 @@
+package com.finaxis.finaxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaxisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaxisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finaxis/finaxis/FinaxisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finaxis.finaxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaxisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
